134eb1fee157c54ecf8a5ed1553ac3a0b2f0f4bb
MrAntex <jul31.chignard@gmail.com>
2024-01-27 04:51:51 -0500

    Random spawn acceleration de la fréquence de spawn

--- main.gd
+++ main.gd
@@ -6,6 +6,9 @@ var rng = RandomNumberGenerator.new()
 const messWidth = 128
 const messHeight = 128
 
+var probaSpawn = 0.05
+var nbSpawned = 0
+
 #var windowWidth = get_viewport().size.x
 #var windowHeight = get_viewport().size.y
 # Called when the node enters the scene tree for the first time.
@@ -17,19 +20,24 @@ func inst(pos):
 	print("Spawned at ", pos)
 
 func _timeout() -> void:
-	var border = rng.randi_range(0,1)
-	var y = rng.randi_range(messHeight/2, get_window().size.y - messHeight/2)
+	if rng.randf() < probaSpawn:
+		var border = rng.randi_range(0,1)
+		var y = rng.randi_range(messHeight/2, get_window().size.y - messHeight/2)
 	
-	var x = messWidth/2 if border == 0 else get_window().size.x - messWidth/2
-	
-	inst(Vector2(x,y))
+		var x = messWidth/2 if border == 0 else get_window().size.x - messWidth/2
+		
+		inst(Vector2(x,y))
+		nbSpawned+=1
+		if nbSpawned%20==0:
+			probaSpawn+=0.01
+			print(probaSpawn," ", nbSpawned)
 	
 func _init():
 	var timer = Timer.new()
 	add_child(timer)
 	timer.autostart = true
 	timer.one_shot = false
-	timer.wait_time = 1
+	timer.wait_time = 0.1
 	timer.connect("timeout", self._timeout)
 
 # Fait apparaitre un messager à la pos de la sours lors d'un clic
